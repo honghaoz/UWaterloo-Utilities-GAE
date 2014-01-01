a74ee42e44944f540e1791da0dd9e97a7e380497
Zhang Honghao <zhh358@gmail.com>
2014-01-01 18:29:43 -0500

    Store the datas to dictionaries
    Due to the Google database datastore write operations limit.
    This version store the data in dictionaries, and then only store the
    watched data in database.

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ import copy
 import sys
 sys.path.insert(0, 'libs')
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 
 # Global variables
 template_dir = os.path.join(os.path.dirname(__file__), 'html_template')
@@ -78,6 +79,62 @@ class Course(ndb.Model):
     #classes = ndb.StructuredProperty(Class, repeated = True)
     created_time = ndb.DateTimeProperty(auto_now_add = True)
     last_modified = ndb.DateTimeProperty(auto_now = True)
+
+# data structures
+class CClass:
+    def __init__(self, id = None, subject = None, catalog_num = None, class_num = None, comp_sec = None, camp_loc = None,
+                       assoc_class = None, rel1 = None, rel2 = None, enrol_cap = None, enrol_tot = None,
+                       wait_cap = None, wait_tot = None, time_data = None, bldg_room = None, 
+                       instructor = None, note = []):
+        self.id = id,
+        self.subject = subject
+        self.catalog_num = catalog_num
+        self.class_num = class_num
+        self.comp_sec = comp_sec
+        self.camp_loc = camp_loc
+        self.assoc_class = assoc_class
+        self.rel1 = rel1
+        self.rel2 = rel2
+        self.enrol_cap = enrol_cap
+        self.enrol_tot = enrol_tot
+        self.wait_cap = wait_cap
+        self.wait_tot = wait_tot
+        self.time_data = time_data
+        self.bldg_room = bldg_room
+        self.instructor = instructor
+        self.note = note
+
+Dic_CClass = OrderedDict()
+def Dic_CClass_get_by_id(id):
+    global Dic_CClass
+    if id in Dic_CClass:
+        return Dic_CClass[id]
+    else:
+        return None
+def Dic_CClass_put(id, CClass_instance):
+    global Dic_CClass
+    Dic_CClass[id] = CClass_instance
+
+class CCourse:
+    def __init__(self, subject = None, catalog_num = None, units = None, title = None, note = None):
+        self.subject = subject
+        self.catalog_num = catalog_num
+        self.units = units
+        self.title = title
+        self.note = note
+        self.classes = []
+        self.created_time = time.localtime()
+
+Dic_CCourse = OrderedDict()
+def Dic_CCourse_get_by_id(id):
+    global Dic_CCourse
+    if id in Dic_CCourse:
+        return Dic_CCourse[id]
+    else:
+        return None
+def Dic_CCourse_put(id, CCourse_instance):
+    global Dic_CCourse
+    Dic_CCourse[id] = CCourse_instance
 
 # Basic Handler
 class ECEHandle(webapp2.RequestHandler):
@@ -213,21 +270,152 @@ class CourseEnrolmentNotifier(ECEHandle):
             self.render_front_page(self.sess_values, self.term_dic, self.subject_values)
 
     # consumes table, subject, catalog_num and return list of Class instances
-    def readClasses(self, table, id, subject, catalog_num):
-        class_num = None
-        comp_sec = ""
-        camp_loc = ""
-        assoc_class = None
-        rel1 = None
-        rel2 = None
-        enrol_cap = 0
-        enrol_tot = 0
-        wait_cap = 0
-        wait_tot = 0
-        time_data = ""
-        bldg_room = ""
-        instructor = ""
+    # def readClasses(self, table, id, subject, catalog_num):
+    #     class_num = None
+    #     comp_sec = ""
+    #     camp_loc = ""
+    #     assoc_class = None
+    #     rel1 = None
+    #     rel2 = None
+    #     enrol_cap = 0
+    #     enrol_tot = 0
+    #     wait_cap = 0
+    #     wait_tot = 0
+    #     time_data = ""
+    #     bldg_room = ""
+    #     instructor = ""
 
+    #     lenOfChildren = len(list(table.children))
+    #     row = 0
+    #     while row < lenOfChildren:
+    #         # escape end line
+    #         tr = table.contents[row]
+    #         if tr == u'\n':
+    #             row += 1
+    #             continue
+    #         elif len(list(tr.children)) == 27 and str(tr.get_text().strip().split()[0]) == 'Class':
+    #             row += 1
+    #             continue
+    #         #elif 9 < len(list(tr.children)) <= 13 and str(tr.get_text().strip().split()[0]).isdigit():
+    #         elif 9 < len(list(tr.children)) <= 13:
+    #             if tr.contents[0].get_text().strip() == u'':
+    #                 c = Class.get_by_id(id + "." + str(class_num))
+    #                 if not tr.contents[10].get_text().strip() == u'':
+    #                     c.time_data += '\n' + str('\n'.join(s.strip() for s in tr.contents[10].strings))
+    #                     c.put()
+    #                 try:
+    #                     if not tr.contents[11].get_text().strip() == u'':
+    #                         c.bldg_room += '\n' + str(tr.contents[11].string.strip())
+    #                         c.put()
+    #                 except:
+    #                     row += 1
+    #                     continue
+    #                 try:
+    #                     if not tr.contents[12].get_text().strip() == u'':
+    #                         c.instructor += '\n' + str(tr.contents[12].string.strip())
+    #                         c.put()
+    #                 except:
+    #                     row += 1
+    #                     continue
+    #                 row += 1
+    #                 continue
+    #             class_num = None
+    #             comp_sec = ""
+    #             camp_loc = ""
+    #             assoc_class = None
+    #             rel1 = None
+    #             rel2 = None
+    #             enrol_cap = 0
+    #             enrol_tot = 0
+    #             wait_cap = 0
+    #             wait_tot = 0
+    #             time_data = ""
+    #             bldg_room = ""
+    #             instructor = ""
+    #             try:
+    #                 class_num = int(tr.contents[0].string.strip())
+    #             except:
+    #                 class_num = None
+    #             try:
+    #                 comp_sec = str(tr.contents[1].string.strip())
+    #             except:
+    #                 comp_sec = None
+    #             try:
+    #                 camp_loc = str(tr.contents[2].string.strip())
+    #             except:
+    #                 camp_loc = None
+    #             try:
+    #                 assoc_class = int(tr.contents[3].string.strip())
+    #             except:
+    #                 assoc_class = None
+    #             try:
+    #                 rel1 = int(tr.contents[4].string.strip())
+    #             except:
+    #                 rel1 = None
+    #             try:
+    #                 rel2 = int(tr.contents[5].string.strip())
+    #             except:
+    #                 rel2 = None
+    #             try:
+    #                 enrol_cap = int(tr.contents[6].string.strip())
+    #             except:
+    #                 enrol_cap = None
+    #             try:
+    #                 enrol_tot = int(tr.contents[7].string.strip())
+    #             except:
+    #                 enrol_tot = None
+    #             try:
+    #                 wait_cap = int(tr.contents[8].string.strip())
+    #             except:
+    #                 wait_cap = None
+    #             try:
+    #                 wait_tot = int(tr.contents[9].string.strip())
+    #             except:
+    #                 wait_tot = None
+    #             try:
+    #                 if not tr.contents[10].br == None:
+    #                     time_data = str('\n'.join(s.strip() for s in tr.contents[10].strings))
+    #                 else:
+    #                     time_data = str(tr.contents[10].string.strip())
+    #             except:
+    #                 time_data = None
+    #             try:
+    #                 bldg_room = str(tr.contents[11].string.strip())
+    #             except:
+    #                 bldg_room = None
+    #             try:
+    #                 instructor = str(tr.contents[12].string.strip())
+    #             except:
+    #                 instructor = None
+    #             Class(id = id + "." + str(class_num),
+    #                   subject = subject,
+    #                   catalog_num = catalog_num,
+    #                   class_num = class_num,
+    #                   comp_sec = comp_sec,
+    #                   camp_loc = camp_loc,
+    #                   assoc_class = assoc_class,
+    #                   rel1 = rel1,
+    #                   rel2 = rel2,
+    #                   enrol_cap = enrol_cap,
+    #                   enrol_tot = enrol_tot,
+    #                   wait_cap = wait_cap,
+    #                   wait_tot = wait_tot,
+    #                   time_data = time_data,
+    #                   bldg_room = bldg_room,
+    #                   instructor = instructor,
+    #                   note = []).put()
+    #             row += 1
+    #         # need revise
+    #         elif len(list(tr.children)) < 9 and (not tr.i == None):
+    #             c = Class.get_by_id(id + "." + str(class_num))
+    #             c.note.append(str(tr.i.string.strip()))
+    #             c.put()
+    #             row += 1
+    #         else:
+    #             row += 1
+    #             continue
+    #     return True
+    def readClasses(self, table, id, subject, catalog_num):
         lenOfChildren = len(list(table.children))
         row = 0
         while row < lenOfChildren:
@@ -242,21 +430,25 @@ class CourseEnrolmentNotifier(ECEHandle):
             #elif 9 < len(list(tr.children)) <= 13 and str(tr.get_text().strip().split()[0]).isdigit():
             elif 9 < len(list(tr.children)) <= 13:
                 if tr.contents[0].get_text().strip() == u'':
-                    c = Class.get_by_id(id + "." + str(class_num))
+                    #c = Class.get_by_id(id + "." + str(class_num))
+                    c = Dic_CClass_get_by_id(id + "." + str(class_num))
                     if not tr.contents[10].get_text().strip() == u'':
+                        #c.time_data += '\n' + str('\n'.join(s.strip() for s in tr.contents[10].strings))
+                        #c.put()
                         c.time_data += '\n' + str('\n'.join(s.strip() for s in tr.contents[10].strings))
-                        c.put()
                     try:
                         if not tr.contents[11].get_text().strip() == u'':
+                            # c.bldg_room += '\n' + str(tr.contents[11].string.strip())
+                            # c.put()
                             c.bldg_room += '\n' + str(tr.contents[11].string.strip())
-                            c.put()
                     except:
                         row += 1
                         continue
                     try:
                         if not tr.contents[12].get_text().strip() == u'':
+                            # c.instructor += '\n' + str(tr.contents[12].string.strip())
+                            # c.put()
                             c.instructor += '\n' + str(tr.contents[12].string.strip())
-                            c.put()
                     except:
                         row += 1
                         continue
@@ -330,29 +522,50 @@ class CourseEnrolmentNotifier(ECEHandle):
                     instructor = str(tr.contents[12].string.strip())
                 except:
                     instructor = None
-                Class(id = id + "." + str(class_num),
-                      subject = subject,
-                      catalog_num = catalog_num,
-                      class_num = class_num,
-                      comp_sec = comp_sec,
-                      camp_loc = camp_loc,
-                      assoc_class = assoc_class,
-                      rel1 = rel1,
-                      rel2 = rel2,
-                      enrol_cap = enrol_cap,
-                      enrol_tot = enrol_tot,
-                      wait_cap = wait_cap,
-                      wait_tot = wait_tot,
-                      time_data = time_data,
-                      bldg_room = bldg_room,
-                      instructor = instructor,
-                      note = []).put()
+                # Class(id = id + "." + str(class_num),
+                #       subject = subject,
+                #       catalog_num = catalog_num,
+                #       class_num = class_num,
+                #       comp_sec = comp_sec,
+                #       camp_loc = camp_loc,
+                #       assoc_class = assoc_class,
+                #       rel1 = rel1,
+                #       rel2 = rel2,
+                #       enrol_cap = enrol_cap,
+                #       enrol_tot = enrol_tot,
+                #       wait_cap = wait_cap,
+                #       wait_tot = wait_tot,
+                #       time_data = time_data,
+                #       bldg_room = bldg_room,
+                #       instructor = instructor,
+                #       note = []).put()
+                newCClass = CClass(id + "." + str(class_num),
+                                   subject,
+                                   catalog_num,
+                                   class_num,
+                                   comp_sec,
+                                   camp_loc,
+                                   assoc_class,
+                                   rel1,
+                                   rel2,
+                                   enrol_cap,
+                                   enrol_tot,
+                                   wait_cap,
+                                   wait_tot,
+                                   time_data,
+                                   bldg_room,
+                                   instructor,
+                                   [])
+                Dic_CClass_put(id + "." + str(class_num), newCClass)
+                Dic_CCourse_get_by_id(id).classes.append(newCClass)
                 row += 1
             # need revise
             elif len(list(tr.children)) < 9 and (not tr.i == None):
-                c = Class.get_by_id(id + "." + str(class_num))
+                # c = Class.get_by_id(id + "." + str(class_num))
+                # c.note.append(str(tr.i.string.strip()))
+                # c.put()
+                c = Dic_CClass_get_by_id(id + "." + str(class_num))
                 c.note.append(str(tr.i.string.strip()))
-                c.put()
                 row += 1
             else:
                 row += 1
@@ -360,12 +573,11 @@ class CourseEnrolmentNotifier(ECEHandle):
         return True
 
     def readCourses(self, table):
-        subject = ""
-        catalog_num = ""
+        subject = None
+        catalog_num = None
         units = None
-        title = ""
-        note = ""
-
+        title = None
+        note = None
         lenOfChildren = len(list(table.children))
         row = 0
         while row < lenOfChildren:
@@ -377,19 +589,19 @@ class CourseEnrolmentNotifier(ECEHandle):
             # course head row
             elif len(list(tr.children)) == 8 and str(tr.get_text().strip().split()[0]) == str(self.subject):
                 #clear old data
-                subject = ""
+                subject = None
                 catalog_num = None
                 units = None
-                title = ""
+                title = None
                 # set new data
                 try:
                     subject = str(tr.contents[0].string.strip())
                 except:
-                    subject = ""
+                    subject = None
                 try:
                     catalog_num = str(tr.contents[2].string.strip())
                 except:
-                    catalog_num = ""
+                    catalog_num = None
                 try:
                     units = float(tr.contents[4].string.strip())
                 except:
@@ -397,23 +609,27 @@ class CourseEnrolmentNotifier(ECEHandle):
                 try:
                     title = str(tr.contents[6].string.strip())
                 except:
-                    title = ""
+                    title = None
                 row += 1
             elif len(list(tr.children)) == 1 and (not tr.get_text().strip() == u'') and tr.get_text().strip().split()[0] == u'Notes:':
-                note = ""
+                note = None
                 try:
                     note = str(tr.get_text().strip())
                 except:
                     note = None
                 row += 1
             elif len(list(tr.children)) == 2 and (not tr.table == None):
-                Course(id = subject + "." + str(catalog_num), 
-                       subject = subject, 
-                       catalog_num = catalog_num, 
-                       units = units,
-                       title = title, 
-                       note = note).put()
-                if self.readClasses(tr.table, subject + "." + catalog_num, subject, catalog_num):
+                # Course(id = subject + "." + str(catalog_num), 
+                #        subject = subject, 
+                #        catalog_num = catalog_num, 
+                #        units = units,
+                #        title = title, 
+                #        note = note).put()
+                if note:
+                    Dic_CCourse_put(subject + "." + str(catalog_num), CCourse(subject, catalog_num, units, title, note))
+                else:
+                    Dic_CCourse_put(subject + "." + str(catalog_num), CCourse(subject, catalog_num, units, title))
+                if self.readClasses(tr.table, subject + "." + str(catalog_num), subject, catalog_num):
                     row += 1
                     continue
                 else:
@@ -455,7 +671,11 @@ class CourseEnrolmentNotifier(ECEHandle):
         query_obj = urllib.urlencode(query_dic)
         # read query result
         if self.readQueryResult(query_url, query_obj):
-            self.write("success!")
+            #self.write("success!")
+            res = ""
+            for k, d in Dic_CClass.items():
+                res += str(d.id) + "\t" + d.subject + "\t" + str(d.catalog_num) + "\t" + str(d.class_num) + "\t" + str(d.comp_sec) + "\t" + str(d.camp_loc) + " " + str(d.assoc_class) + "\t" + str(d.rel1) + "\t" + str(d.rel2) + "\t" + str(d.enrol_cap) + "\t" + str(d.enrol_tot) + "\t" + str(d.wait_cap) + "\t" + str(d.wait_tot) + "\t" + str(d.time_data) + "\t" + str(d.bldg_room) + "\t" + str(d.instructor) + "\n" 
+            self.write(res)
         else:
             self.write("Failed!")
 
